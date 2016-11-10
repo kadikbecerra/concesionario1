ab265cc1ef83047f4ca3d8e41e4eab43bc905d21
wKassia <Wilson@192.168.1.9>
2016-11-10 17:19:37 -0400

    Primera ejecucion java

---
+++ Concesionario/src/Principal/Principal.java
@@ -0,0 +1,12 @@
+package Principal;
+
+import interfaz.VentanaPrincipal;
+public class Principal {
+	
+	public static void main(String [] arg){
+		
+		new VentanaPrincipal().setAlwaysOnTop(true);
+		
+	}
+
+}
--- Concesionario/src/interfaz/VentanaPrincipal.java
+++ Concesionario/src/interfaz/VentanaPrincipal.java
@@ -16,19 +16,7 @@ public class VentanaPrincipal extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaPrincipal frame = new VentanaPrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	
 	/**
 	 * Create the frame.
 	 */
